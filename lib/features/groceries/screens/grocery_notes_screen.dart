bb906403dd24a54a8e2bf36cfdab6322a8bbbf25
AuzaAl <auzaganteng007@gmail.com>
2026-06-19 15:58:38 +0700

    refactor: restructure assets, migrate to feature-based architecture, and initialize Firebase
    - Move fonts and images from lib/assets/ to root assets/
    - Refactor screens into lib/features/ (splash, groceries)
    - Add Firebase initialization in main.dart
    - Update Firebase and dotenv dependencies

--- lib/features/groceries/screens/grocery_notes_screen.dart
+++ lib/features/groceries/screens/grocery_notes_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../providers/grocery_provider.dart';
+import '../../../providers/grocery_provider.dart';
 
 class GroceryNotesScreen extends ConsumerStatefulWidget {
   const GroceryNotesScreen({super.key});
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'features/splash/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     const ProviderScope(
       child: MainApp(),
